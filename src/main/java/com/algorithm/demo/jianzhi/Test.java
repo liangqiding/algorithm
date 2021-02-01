68ec5b84e0cbec7e4c5a0f2c1ec7e761b306b86d
qidingliang <742740345@qq.com>
2021-02-01 16:50:13 +0800

    1.25练习

---
+++ src/main/java/com/algorithm/demo/jianzhi/Test.java
@@ -0,0 +1,15 @@
+package com.algorithm.demo.jianzhi;
+
+/**
+ * date: 2021-02-01 15:55
+ * description
+ *
+ * @author qiDing
+ */
+public class Test {
+    public static void main(String[] args) {
+        int a=-10;
+        String s = Integer.toBinaryString(a);
+        System.out.println(s);
+    }
+}
