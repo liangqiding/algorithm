8d2c22074e8c98a33653a3977649110bc8dc46f4
qidingliang <742740345@qq.com>
2021-02-02 16:56:30 +0800

    1.25练习

--- src/main/java/com/algorithm/demo/jianzhi/Test.java
+++ src/main/java/com/algorithm/demo/jianzhi/Test.java
@@ -8,8 +8,6 @@ package com.algorithm.demo.jianzhi;
  */
 public class Test {
     public static void main(String[] args) {
-        int a=-10;
-        String s = Integer.toBinaryString(a);
-        System.out.println(s);
+
     }
 }
